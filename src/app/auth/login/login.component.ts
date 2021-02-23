514dfbc59236ed7f57176590102f34fc5cfe9bd3
shnur42 <andrei.chernyshov@deliveryweb.ru>
2021-02-23 20:09:55 +0300

    (lesson 6) feat: Defining NgRx Actions using Action Creators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
 import {Store} from '@ngrx/store';
-
 import {AuthService} from '../auth.service';
 import {tap} from 'rxjs/operators';
 import {noop} from 'rxjs';
 import {Router} from '@angular/router';
+import { AppState } from '../store/reducers';
+import { login } from '../store/auth.actions';
 
 @Component({
   selector: 'login',
@@ -20,12 +20,15 @@ export class LoginComponent implements OnInit {
   constructor(
       private fb: FormBuilder,
       private auth: AuthService,
-      private router: Router) {
+      private router: Router,
+      private store: Store<AppState>
+  ) {
 
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
           password: ['test', [Validators.required]]
       });
+
 
   }
 
@@ -34,7 +37,19 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-
+    const formVal = this.form.value;
+    this.auth.login(formVal.email, formVal.password)
+      .pipe(
+        tap(user => {
+          console.log(user);
+          this.store.dispatch(login({ user }));
+          this.router.navigateByUrl('/courses');
+        })
+      )
+      .subscribe(
+        noop,
+        () => alert('Login Failed')
+      );
   }
 
 }

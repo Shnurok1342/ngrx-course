59be29f5fc0dc54aa38e74400b2109656cff2869
Shnurok1342 <a13bepb@yandex.ru>
2021-02-24 23:23:12 +0300

    (lesson 43) feat: NgRx Data CRUD - Optimistic Delete Implementation

--- src/app/courses/courses-card-list/courses-card-list.component.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.ts
@@ -1,51 +1,42 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Course} from '../model/course';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import {MatDialog} from '@angular/material/dialog';
 import {EditCourseDialogComponent} from '../edit-course-dialog/edit-course-dialog.component';
 import {defaultDialogConfig} from '../shared/default-dialog-config';
+import {CourseEntityService} from '../services/course-entity.service';
 
 @Component({
-    selector: 'app-courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+  selector: 'app-courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css']
 })
 export class CoursesCardListComponent implements OnInit {
+  @Input() courses: Course[];
+  @Output() courseChanged = new EventEmitter();
 
-    @Input()
-    courses: Course[];
+  constructor(
+    private dialog: MatDialog,
+    private courseService: CourseEntityService
+  ) {}
 
-    @Output()
-    courseChanged = new EventEmitter();
+  ngOnInit() {}
 
-    constructor(
-      private dialog: MatDialog ) {
-    }
+  editCourse(course: Course) {
+    const dialogConfig = defaultDialogConfig();
+    dialogConfig.data = {
+      dialogTitle: 'Edit Course',
+      course,
+      mode: 'update'
+    };
 
-    ngOnInit() {
-
-    }
-
-    editCourse(course: Course) {
-
-        const dialogConfig = defaultDialogConfig();
-
-        dialogConfig.data = {
-          dialogTitle: 'Edit Course',
-          course,
-          mode: 'update'
-        };
-
-        this.dialog.open(EditCourseDialogComponent, dialogConfig)
-          .afterClosed()
-          .subscribe(() => this.courseChanged.emit());
-
-    }
-
-  onDeleteCourse(course: Course) {
-
-
+    this.dialog.open(EditCourseDialogComponent, dialogConfig)
+      .afterClosed()
+      .subscribe(() => this.courseChanged.emit());
   }
 
+  onDeleteCourse(course: Course) {
+    this.courseService.delete(course);
+  }
 }
 
 

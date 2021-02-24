2069db0ae686b36d5af2c8a006adfad22e8af583
Shnurok1342 <a13bepb@yandex.ru>
2021-02-24 23:39:36 +0300

    (lessons 44-45) feat: Setting Up a new Enttity - The Lesson Entity and Implementing the Course Component Using NgRx Data

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,11 +1,10 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Lesson} from '../model/lesson';
-import {concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom} from 'rxjs/operators';
-import {CoursesHttpService} from '../services/courses-http.service';
-
+import {map} from 'rxjs/operators';
+import {CourseEntityService} from '../services/course-entity.service';
 
 @Component({
   selector: 'app-course',
@@ -13,37 +12,24 @@ import {CoursesHttpService} from '../services/courses-http.service';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-
   course$: Observable<Course>;
-
   lessons$: Observable<Lesson[]>;
-
   displayedColumns = ['seqNo', 'description', 'duration'];
 
-  nextPage = 0;
-
   constructor(
-    private coursesService: CoursesHttpService,
-    private route: ActivatedRoute) {
-
-  }
+    private route: ActivatedRoute,
+    private coursesService: CourseEntityService
+  ) {}
 
   ngOnInit() {
-
     const courseUrl = this.route.snapshot.paramMap.get('courseUrl');
-
-    this.course$ = this.coursesService.findCourseByUrl(courseUrl);
-
-    this.lessons$ = this.course$.pipe(
-      concatMap(course => this.coursesService.findLessons(course.id)),
-      tap(console.log)
-    );
-
+    this.course$ = this.coursesService.entities$
+      .pipe(
+        map(courses => courses.find(c => c.url === courseUrl))
+      );
+    this.lessons$ = of([]);
   }
 
-
-  loadLessonsPage(course: Course) {
-
-  }
+  loadLessonsPage(course: Course) {}
 
 }
